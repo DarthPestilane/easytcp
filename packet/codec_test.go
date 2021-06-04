754d63cca0ca1c48bcd9d7e242b98a6ed5161b07
DarthPestilane <wangrui.superman@gmail.com>
2021-06-04 10:00:03 +0800

    improve codec test

--- packet/codec_test.go
+++ packet/codec_test.go
@@ -5,18 +5,40 @@ import (
 	"testing"
 )
 
-func TestDefaultCodec_Encode(t *testing.T) {
+func TestStringCodec_Encode(t *testing.T) {
 	c := &StringCodec{}
 	b, err := c.Encode("hello")
 	assert.NoError(t, err)
 	assert.Equal(t, []byte("hello"), b)
 }
 
-func TestDefaultCodec_Decode(t *testing.T) {
+func TestStringCodec_Decode(t *testing.T) {
 	c := &StringCodec{}
 	data := []byte("hello")
 	var v string
-	err := c.Decode(data, &v)
-	assert.NoError(t, err)
+	assert.NoError(t, c.Decode(data, &v))
 	assert.Equal(t, string(data), v)
+
+	var v2 int
+	assert.Error(t, c.Decode(data, &v2))
+}
+
+func TestJsonCodec_Decode(t *testing.T) {
+	c := &JsonCodec{}
+	data := []byte(`{"id": 1}`)
+	var v struct {
+		Id int `json:"id"`
+	}
+	assert.NoError(t, c.Decode(data, &v))
+	assert.EqualValues(t, v.Id, 1)
+}
+
+func TestJsonCodec_Encode(t *testing.T) {
+	c := &JsonCodec{}
+	v := struct {
+		Id int `json:"id"`
+	}{Id: 1}
+	b, err := c.Encode(v)
+	assert.NoError(t, err)
+	assert.JSONEq(t, string(b), `{"id": 1}`)
 }
