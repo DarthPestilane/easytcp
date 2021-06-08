e1a18e3d63bae5c0c69a8b49a103d74989b6c05a
DarthPestilane <wangrui.superman@gmail.com>
2021-06-08 17:30:47 +0800

    doc for Server interface

--- server/server.go
+++ server/server.go
@@ -4,9 +4,21 @@ import (
 	"github.com/DarthPestilane/easytcp/router"
 )
 
+// Server is a generic network server.
 type Server interface {
+	// Serve starts to serving at the addr.
+	// Returns error when error occurred.
 	Serve(addr string) error
+
+	// Stop stops the server from serving.
+	// All the goroutines created from Server should be exit.
+	// Returns error when error occurred.
 	Stop() error
+
+	// Use registers a list of global middlewares.
 	Use(middlewares ...router.MiddlewareFunc)
+
+	// AddRoute registers a handler for the message matches msgId.
+	// Also registers middlewares.
 	AddRoute(msgId uint, handler router.HandlerFunc, middlewares ...router.MiddlewareFunc)
 }
