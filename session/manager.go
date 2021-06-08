da660d3ae3ed25dab5b304ff9bed02ecc98bf9ed
DarthPestilane <wangrui.superman@gmail.com>
2021-06-08 16:50:45 +0800

    add comment doc for session manager

--- session/manager.go
+++ session/manager.go
@@ -9,13 +9,15 @@ var (
 	manager     *Manager
 )
 
-// Manager 会话管理器，维护运行时产生的所有 Session
+// Manager manages all the sessions in application runtime.
 type Manager struct {
-	// Sessions 所有 Session. key 是 Session.ID(), value 是 Session
+
+	// Sessions keeps all sessions.
+	// Key is session's ID, value is Session
 	Sessions sync.Map
 }
 
-// Sessions 得到 Manager 实例
+// Sessions returns a Manager pointer in a singleton way.
 func Sessions() *Manager {
 	managerOnce.Do(func() {
 		manager = &Manager{}
@@ -23,6 +25,8 @@ func Sessions() *Manager {
 	return manager
 }
 
+// Add adds a session to Sessions.
+// If the ID of s already existed in Sessions, it replaces the value with the s.
 func (m *Manager) Add(s Session) {
 	if s == nil {
 		return
@@ -30,10 +34,14 @@ func (m *Manager) Add(s Session) {
 	m.Sessions.Store(s.ID(), s)
 }
 
+// Remove removes a session from Sessions.
+// Parameter id should be the session's id.
 func (m *Manager) Remove(id string) {
 	m.Sessions.Delete(id)
 }
 
+// Get returns a Session when found by the id,
+// returns nil otherwise.
 func (m *Manager) Get(id string) Session {
 	sess, ok := m.Sessions.Load(id)
 	if !ok {
@@ -42,6 +50,8 @@ func (m *Manager) Get(id string) Session {
 	return sess.(Session)
 }
 
+// Range calls fn sequentially for each id and sess present in the Sessions.
+// If fn returns false, range stops the iteration.
 func (m *Manager) Range(fn func(id string, sess Session) (next bool)) {
 	m.Sessions.Range(func(key, value interface{}) bool {
 		return fn(key.(string), value.(Session))
