9681738cd58f53373c118286e5cd69d52e5b6595
zxl <darthminion@163.com>
2021-12-30 10:10:21 +0800

    feat: the default logger is mute

--- logger.go
+++ logger.go
@@ -2,9 +2,15 @@ package easytcp
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 )
+
+var _ Logger = &DefaultLogger{}
+
+// Log is the instance of Logger interface.
+var Log Logger = newMuteLogger()
 
 // Logger is the generic interface for log recording.
 type Logger interface {
@@ -12,21 +18,22 @@ type Logger interface {
 	Tracef(format string, args ...interface{})
 }
 
-// DefaultLogger is the default logger instance for this package.
-// DefaultLogger uses the built-in log.Logger.
-type DefaultLogger struct {
-	rawLogger *log.Logger
-}
-
-var _ Logger = &DefaultLogger{}
-
-// Log is the instance of Logger interface.
-var Log Logger = newLogger()
-
 func newLogger() *DefaultLogger {
 	return &DefaultLogger{
 		rawLogger: log.New(os.Stdout, "easytcp ", log.Ldate|log.Ltime|log.Lmicroseconds|log.Lmsgprefix),
 	}
+}
+
+func newMuteLogger() *DefaultLogger {
+	return &DefaultLogger{
+		rawLogger: log.New(ioutil.Discard, "easytcp", log.LstdFlags),
+	}
+}
+
+// DefaultLogger is the default logger instance for this package.
+// DefaultLogger uses the built-in log.Logger.
+type DefaultLogger struct {
+	rawLogger *log.Logger
 }
 
 // Errorf implements Logger Errorf method.
